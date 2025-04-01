a4b31f5b3a2be5e5507eea93dcf141aff0c1e1a2
🍆 가지 <gajicoding@gmail.com>
2025-04-01 14:39:59 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.52 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,10 +1,6 @@
 class Solution {
-    public static int fun(int a, int b){
-		   return Integer.parseInt(Integer.toString(a)+Integer.toString(b));
-	}
-    
     public int solution(int a, int b) {
-        int answer = fun(a, b) > fun(b, a) ? fun(a, b) : fun(b, a);
+        int answer = Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
         return answer;
     }
 }
