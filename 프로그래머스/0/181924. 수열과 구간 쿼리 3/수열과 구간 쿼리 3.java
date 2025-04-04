1665181c480eb356b8ca0317a23a1b5f803b1e5f
🍆 가지 <gajicoding@gmail.com>
2025-04-04 13:12:50 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr.clone();
+        int temp;
+        
+        for(int[] q: queries){
+            temp = answer[q[0]];
+            answer[q[0]] = answer[q[1]];
+            answer[q[1]] = temp;
+        }
+        return answer;
+    }
+}
