16ce8e84070b6c378b6b98888dc4218c2829a971
🍆 가지 <gajicoding@gmail.com>
2025-04-07 20:45:25 +0900

    [level 0] Title: 배열 만들기 4, Time: 52.95 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        int i = 0;
+
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.push(arr[i]);
+                i++;
+            } else if (stk.peek() < arr[i]) {
+                stk.push(arr[i]);
+                i++;
+            } else {
+                stk.pop();
+            }
+        }
+
+        int[] answer = new int[stk.size()];
+        for (int j = stk.size() - 1; j >= 0; j--) {
+            answer[j] = stk.pop();
+        }
+
+        return answer;
+    }
+}
