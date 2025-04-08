4a6e4df1443ff9f06db4168d8218f2e2bab05076
🍆 가지 <gajicoding@gmail.com>
2025-04-08 16:49:27 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.54 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -2,53 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int[] counts = new int[7];
-        counts[a]++;
-        counts[b]++;
-        counts[c]++;
-        counts[d]++;
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
 
-        List<Integer> values = new ArrayList<>();
-        for (int i = 1; i <= 6; i++) {
-            if (counts[i] > 0) {
-                values.add(i);
-            }
+        int answer = 0;
+
+        if (dice[0] == dice[3]) {
+            answer = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            answer = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            answer = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            answer = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            answer = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            answer = dice[0] * dice[1];
+        } else {
+            answer = dice[0];
         }
 
-        int size = values.size();
-
-        if (size == 1) {
-            return 1111 * values.get(0);
-        }
-
-        if (size == 2) {
-            int first = values.get(0);
-            int second = values.get(1);
-            int cnt1 = counts[first];
-            int cnt2 = counts[second];
-
-            if (cnt1 == 3 || cnt2 == 3) {
-                int p = cnt1 == 3 ? first : second;
-                int q = cnt1 == 1 ? first : second;
-                return (int) Math.pow(10 * p + q, 2);
-            } else {
-                return (first + second) * Math.abs(first - second);
-            }
-        }
-
-        if (size == 3) {
-            int pair = 0, single1 = 0, single2 = 0;
-            for (int i = 1; i <= 6; i++) {
-                if (counts[i] == 2) {
-                    pair = i;
-                } else if (counts[i] == 1) {
-                    if (single1 == 0) single1 = i;
-                    else single2 = i;
-                }
-            }
-            return single1 * single2;
-        }
-
-        return Math.min(Math.min(a, b), Math.min(c, d));
+        return answer;
     }
 }
