cafd2fa7cf46a50826696fc697b540f8b7240cfc
🍆 가지 <gajicoding@gmail.com>
2025-04-09 09:54:11 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-          
-        for(int i=0; i<my_string.length(); i++){
-            if(my_string.substring(i).equals(is_suffix)){
-                answer++;
-                break;
-            }
-        }
-        
-        return answer;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
