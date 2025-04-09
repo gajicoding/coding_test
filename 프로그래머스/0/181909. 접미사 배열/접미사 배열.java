abe20338325d480f03d0c54b292a7a77586e9309
🍆 가지 <gajicoding@gmail.com>
2025-04-09 09:39:05 +0900

    [level 0] Title: 접미사 배열, Time: 0.62 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -2,18 +2,14 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        ArrayList<String> list = new ArrayList<>();
+        String[] answer = new String[my_string.length()];
         
         for(int i=0; i<my_string.length(); i++){
-            list.add(my_string.substring(i));
+            answer[i] = (my_string.substring(i));
         }
         
-        list.sort(Comparator.naturalOrder());
+        Arrays.sort(answer);
         
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
         return answer;
     }
 }
