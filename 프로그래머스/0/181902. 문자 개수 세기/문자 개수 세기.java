b7df61fa728d04c6407eba24d8b97e57019ac7c0
🍆 가지 <gajicoding@gmail.com>
2025-04-11 15:00:24 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.53 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        Arrays.fill(answer, 0);
         
         char s;
         for(int i=0; i<my_string.length(); i++){
             s = my_string.charAt(i);
             if(Character.isUpperCase(s)){
-                answer[s-65]++;
+                answer[s-'A']++;
             } else {
-                answer[s-97+26]++;
+                answer[s-'a'+26]++;
             }
         }
         
