2dcf833b95dd0c3288c180e753efa5097401941b
🍆 가지 <gajicoding@gmail.com>
2025-04-14 12:20:05 +0900

    [level 0] Title: 2의 영역, Time: 2.51 ms, Memory: 99.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+         int start = -1, end = -1;
+
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == 2) {
+                if(start == -1) start = i;
+                
+                end = i;
+            }
+        }
+
+        return start == -1 ? new int[]{-1} : Arrays.copyOfRange(arr, start, end+1);
+    }
+}
