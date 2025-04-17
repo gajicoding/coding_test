f5991c053231379872321398b094b891a25ca178
🍆 가지 <gajicoding@gmail.com>
2025-04-17 14:11:28 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int[] arr = num_list.clone();
+        int i = 0;
+        
+        for(int a: arr){
+            while(a != 1){
+                if(a%2 == 0) a/=2;
+                else a = (a-1)/2;
+                i++;
+            }
+        }
+        
+        return i;
+    }
+}
