08fe80b795bed5f1b945f00f89f99ba320c5b23c
🍆 가지 <gajicoding@gmail.com>
2025-04-17 14:01:38 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 15.20 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = arr;
+        int[] answer = arr.clone();
         for(int i=0; i<answer.length; i++){
             if(answer[i] >= 50 && answer[i]%2 == 0){
                 answer[i] /= 2;
