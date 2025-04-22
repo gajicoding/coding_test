56ac7c6165a6e6a4f6fd95b3faaf91edeba2d356
🍆 가지 <gajicoding@gmail.com>
2025-04-22 20:07:08 +0900

    [level 0] Title: 문자열 정수의 합, Time: 1.89 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String num_str) {
+        return num_str.chars()
+                      .map(c -> c - '0')
+                      .sum();
+    }
+}
