f7060428df861d941508e1e7ff3eee2dcee1d7fd
🍆 가지 <gajicoding@gmail.com>
2025-04-23 15:53:03 +0900

    [level 0] Title: 옹알이 (1), Time: 3.62 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,8 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         return Arrays.stream(babbling)
-                        .map(s -> s.replace("aya", "+").replace("ye", "+").replace("woo", "+").replace("ma", "+").replace("+", ""))
-                        .filter(s->s.equals(""))
+                        .map(s -> s.replace("aya", "+")
+                                    .replace("ye", "+")
+                                    .replace("woo", "+")
+                                    .replace("ma", "+").replace("+", "")
+                        ).filter(s->s.equals(""))
                         .toArray(String[]::new).length;
     }
 }
