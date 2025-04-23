a887d874496eb0a3b0db9175b1513ff927e70e14
🍆 가지 <gajicoding@gmail.com>
2025-04-23 23:54:03 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        // 총 몇층 몇번째
+        int totalF = getF(w, n-1);
+        int topMaxIndex = getIndex(w, totalF, n-1);
+        
+        // 선택한 상자
+        int numF = getF(w, num-1);
+        int numIndex = getIndex(w, numF, num-1);
+        
+        int count = (totalF - numF);
+        if (totalF % 2 == 0) {
+            count += (topMaxIndex >= numIndex) ? 1 : 0;
+        } else {
+            count += (topMaxIndex <= numIndex) ? 1 : 0;
+        }
+        
+        return count;
+    }
+    
+    private int getF(int w, int n) {
+        return n/w;
+    }
+    
+    private int getIndex(int w, int f, int n) {
+        if(f % 2 == 0){
+            return n%w;
+        } else {
+            return w - 1 - (n % w);
+        }
+    }
+}
