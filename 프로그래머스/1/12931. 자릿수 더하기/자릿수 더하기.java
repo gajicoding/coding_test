89a3a002ff7f8df2048c956e95cf6c16bf1d4234
🍆 가지 <gajicoding@gmail.com>
2025-04-24 23:11:33 +0900

    [level 1] Title: 자릿수 더하기, Time: 17.75 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,6 +4,6 @@ public class Solution {
     public int solution(int n) {
         return Arrays.stream((n+"").split(""))                  // int->String 변환 후 한 글자씩 자르기
                         .mapToInt(s -> Integer.parseInt(s))     // int로 변환
-                        .sum();                                 // 합
+                        .sum();                                 // 합 
     }
 }
