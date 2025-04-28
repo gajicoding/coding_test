93206534b1f02860b3b3a54d3f5ae325778f7489
🍆 가지 <gajicoding@gmail.com>
2025-04-28 19:30:51 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,5 +1,7 @@
 class Solution {
     public String solution(int num) {
-        return num%2 == 0 ? "Even" : "Odd";
+        // return num%2 == 0 ? "Even" : "Odd";
+        
+        return (num&1) == 0 ? "Even" : "Odd";
     }
 }
