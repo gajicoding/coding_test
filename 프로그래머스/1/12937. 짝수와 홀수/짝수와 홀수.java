3218347d16cee9b042309cdc597d94264943a5a2
🍆 가지 <gajicoding@gmail.com>
2025-04-29 18:59:41 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -4,7 +4,7 @@ class Solution {
     public String solution(int num) {
         // return num%2 == 0 ? "Even" : "Odd";
         
-        // return (num&1) == 0 ? "Even" : "Odd";
+        return (num&1) == 0 ? "Even" : "Odd";
         /*
             num = 3, 11 & 01 = 01 == 1
             num = 4, 100 & 001 = 000 == 0
@@ -12,13 +12,19 @@ class Solution {
         */
         
         // return num/2*2 == num ? "Even" : "Odd";
+    
         
-        return IntStream.of(num).allMatch(n -> n % 2 == 0) ? "Even" : "Odd";
+        
+        
+        
+        // return IntStream.of(num).allMatch(n -> n % 2 == 0) ? "Even" : "Odd";
         
         // return IntStream.of(num).noneMatch(n -> n % 2 == 1) ? "Even" : "Odd";
         
-        // allMatch(): 모두 만족하면 true
-        // anyMatch(): 하나라도 만족하면 true 
-        // noneMatch(): 모두 만족하지 않으면 true
+        /*
+            allMatch(): 모두 만족하면 true
+            anyMatch(): 하나라도 만족하면 true 
+            noneMatch(): 모두 만족하지 않으면 true
+        */
     }
 }
