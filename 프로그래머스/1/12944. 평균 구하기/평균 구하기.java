2cfb9dbc2ab52ed896a7dc0a43e8d7eb016d4152
🍆 가지 <gajicoding@gmail.com>
2025-04-30 18:49:34 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,7 +1,14 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public double solution(int[] arr) {
-        return Arrays.stream(arr).average().orElse(0);
+        int sum = 0;
+        for(int a: arr) sum += a;
+        return (double)sum/arr.length;                        // 0.08ms
+        
+        // return IntStream.of(arr).average().orElse(0);      // 2.13ms
+        
+        // return Arrays.stream(arr).average().orElse(0);  // 3.76ms
     }
 }
