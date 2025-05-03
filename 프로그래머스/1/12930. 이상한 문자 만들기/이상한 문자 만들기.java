f491961c85a19a3b8a3ce7cbfea78278b60019a0
🍆 가지 <gajicoding@gmail.com>
2025-05-03 10:13:20 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.82 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,14 +1,38 @@
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String s) {
-        String answer = "";
+//         String[] arr = s.split("", -1);
+//         String answer = "";
         
-        for (int i=0, j = 0; i<s.length(); i++) {
-            answer += (j++)%2 == 0 ? (s.charAt(i)+"").toUpperCase() : (s.charAt(i)+"").toLowerCase();
+//         for (int i=0, j = 0; i<arr.length; i++) {
+//             answer += (j++) % 2 == 0 ? arr[i].toUpperCase() : arr[i].toLowerCase();
             
-            if((s.charAt(i)+"").equals(" ")){
-                j = 0;
-            }
-        }
-        return answer;
+//             if(arr[i].equals(" ")){
+//                 j = 0;
+//             }
+//         }
+//         return answer;
+        
+//         String[] arr = s.split("", -1);
+//         int[] j = {0};
+//         return IntStream.range(0, s.length())
+//                 .mapToObj(i -> {
+//                     if (arr[i].equals(" ")) {
+//                         j[0] = 0;
+//                         return " ";
+//                     }
+//                     return (j[0]++) % 2 == 0 ? arr[i].toUpperCase() : arr[i].toLowerCase();
+//                 })
+//                 .collect(Collectors.joining());
+        
+        String[] arr = s.split("", -1);
+        int[] j = {0};
+        return IntStream.range(0, s.length())
+                    .mapToObj(i -> arr[i].equals(" ") 
+                        ? (j[0] = 0) == 0 ? " " : " "
+                        : (j[0]++ % 2 == 0) ? arr[i].toUpperCase() : arr[i].toLowerCase())
+                    .collect(Collectors.joining());
     }
 }
