b07f1b6664760c60614b0639745090bc3c1cee4c
🍆 가지 <gajicoding@gmail.com>
2025-05-04 11:56:54 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,6 +1,26 @@
 class Solution {
     public long solution(long n) {
-        long s = (long)Math.sqrt(n);
-        return s*s== n ? (s+1)*(s+1) : -1;
+        // 0.03 ~ 0.08 ms
+        // long s = (long)Math.sqrt(n);
+        // return s*s== n ? (s+1)*(s+1) : -1;
+        
+        
+        long low = 1, high = n;
+        
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            
+            if (mid == (double)n / mid) {
+                return (mid + 1) * (mid + 1);
+            }
+            
+            if (mid < n / mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return -1;
     }
 }
