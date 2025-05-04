613e38950d7296b681163702f413185368004f6c
🍆 가지 <gajicoding@gmail.com>
2025-05-04 10:13:40 +0900

    [level 1] Title: 삼총사, Time: 7.88 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,17 +1,26 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] number) {
-        int count = 0;
+//         int count = 0;
         
-        for(int i=0; i<number.length-2; i++){
-            for(int j=i+1; j<number.length-1; j++){
-                for(int k=j+1; k<number.length; k++){
-                    if(number[i] + number[j] + number[k] == 0){
-                        count++;
-                    }
-                }    
-            }
-        }
+//         for(int i=0; i<number.length-2; i++){
+//             for(int j=i+1; j<number.length-1; j++){
+//                 for(int k=j+1; k<number.length; k++){
+//                     if(number[i] + number[j] + number[k] == 0){
+//                         count++;
+//                     }
+//                 }    
+//             }
+//         }
 
-        return count;
+//         return count;
+        
+        
+        return (int)IntStream.range(0, number.length-2)
+            .flatMap(i -> IntStream.range(i+1, number.length-1)
+                .flatMap(j -> IntStream.range(j+1, number.length)
+                        .filter(k -> number[i] + number[j] + number[k] == 0)))
+            .count();
     }
 }
