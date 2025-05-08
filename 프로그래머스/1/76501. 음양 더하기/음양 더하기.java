b4b3ae802f34b4e591b8163b459a0bfadaa537e4
🍆 가지 <gajicoding@gmail.com>
2025-05-08 19:13:20 +0900

    [level 1] Title: 음양 더하기, Time: 2.01 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,9 +1,20 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 0;
-        for (int i=0; i<signs.length; i++){
-            answer += signs[i] ? absolutes[i] : -1*absolutes[i];
-        }
-        return answer;
+        // 0.04 ~ 0.07 ms
+        // int answer = 0;
+        // for (int i=0; i<signs.length; i++){
+        //     answer += signs[i] ? absolutes[i] : -1*absolutes[i];
+        // }
+        // return answer;
+        
+        // 1.67 ~ 2.58 ms
+        // return IntStream.range(0, signs.length)
+        //                 .map(i -> signs[i] ? absolutes[i] : -1*absolutes[i])
+        //                 .sum();
+        
+        return IntStream.range(0, signs.length)
+                        .reduce(0, (acc, i) -> acc + (signs[i] ? absolutes[i] : -1*absolutes[i]));
     }
 }
