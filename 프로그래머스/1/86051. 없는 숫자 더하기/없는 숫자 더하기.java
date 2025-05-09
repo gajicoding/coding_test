81c920fbeb35ccdb02d2a899e39f4c83fe006709
🍆 가지 <gajicoding@gmail.com>
2025-05-09 19:03:44 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.57 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,13 +1,8 @@
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] numbers) {
-        // int answer = 45;
-        int answer = IntStream.rangeClosed(0, 9).sum();
-            
-        for (int n: numbers){
-            answer -= n;
-        }
-        return answer;
+        return 45 - Arrays.stream(numbers).sum();
     }
 }
