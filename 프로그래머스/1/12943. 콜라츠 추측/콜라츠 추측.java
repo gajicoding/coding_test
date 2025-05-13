5543f6f31cff656ac3c209e63be3a094ffce9315
🍆 가지 <gajicoding@gmail.com>
2025-05-13 19:18:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,7 +3,7 @@ class Solution {
         long n = num;
         for (int i = 0; i <= 500; i++) {
             if (n == 1) return i;
-            n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
+            n = (n % 2 == 0) ? n/2 : n*3 + 1;
         }
         return -1;
     }
