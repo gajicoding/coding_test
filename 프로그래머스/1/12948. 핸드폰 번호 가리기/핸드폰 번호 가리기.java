b620f85ab51362ec49f802041717495b5fa95587
🍆 가지 <gajicoding@gmail.com>
2025-05-14 19:00:56 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.56 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,6 +1,12 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public String solution(String phone_number) {
-        int length = phone_number.length();
-        return "*".repeat(length - 4) + phone_number.substring(length - 4);
+        return "*".repeat(phone_number.length() - 4) + phone_number.substring(phone_number.length() - 4);
+        
+        // return IntStream.range(0, phone_number.length())
+        //         .mapToObj(i -> i < length - 4 ? "*" : String.valueOf(phone_number.charAt(i)))
+        //         .reduce("", (a, b) -> a + b);
+            
     }
 }
